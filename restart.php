b351118613d54b277af3c01c04ca6fa8831b4518
Alex Childers <aaachilders@gmail.com>
2019-04-13 14:46:05 -0700

    Fixed restart.php so that regenerating session ID should work.

--- restart.php
+++ restart.php
@@ -7,8 +7,11 @@ function restart()
 	session_destroy(); 
 	
 	// restart a new session from the landing page 
-	session_regenerate_id(); 
+	session_regenerate_id(TRUE); 
 	session_start(); 
+	
+	// IF THIS DOESN'T WORK, INCLUDE A CALL TO THE FUNCTION THAT CREATES THE 
+	// LANDING PAGE 
 }
 	
 ?>
