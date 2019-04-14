9cc07d6b03ed07142ec73460049a7b7ad5110157
Alex Childers <aaachilders@gmail.com>
2019-04-13 21:36:12 -0700

    Added other_reason.php

---
+++ other_reason.php
@@ -0,0 +1,18 @@
+<?php
+// Allows user to input what occurred if caller's reason for calling was 
+// "other."
+
+function other_reason()
+{
+	?>
+	<form action="<?= htmlentities($_SERVER['PHP_SELF'], ENT_QUOTES) ?>"
+          method="post">
+		<fieldset>
+		<label> State why the caller was calling and what information you gave them.
+			<textarea rows="5" cols="20"> </textarea>
+		</label>
+		</fieldset>
+	</form>
+	<?php
+}
+?>
