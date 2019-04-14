f9616566eeb2ed236e9451f56b5b0ded6d9ef38a
Alex Childers <aaachilders@gmail.com>
2019-04-14 12:55:00 -0700

    Attempt to implement login before proceeding.

--- oci_inserts.php
+++ oci_inserts.php
@@ -2,8 +2,6 @@
 // inserts appropriate information from the $_SESSION array to the database
 // for demo purposes, we'll only deal with the "youth" and "inquiry_demo" table 
 
-insert into [table] (col1, col2, col3)
-values
-('', '', '')
+// first, insert information into youth table 
 
 ?>
--- restart.php
+++ restart.php
@@ -9,8 +9,9 @@ function restart()
 	// restart a new session from the landing page 
 	session_start(); 
 	
-	require_once("Startingpoint.html"); 
-	$_SESSION["next-step"] = "contact-reason"; 
+	<p> Inquiry record successfully submitted. Thank you! </p> 
+	create_login(); 
+	$_SESSION["next-step"] = "start-form"; 
 }
 	
 ?>
